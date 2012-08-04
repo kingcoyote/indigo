c8dfc2170cb7ff4814cecb8d735aa58ef716f452
Steve Phillips <kingcoyote85@gmail.com>
2012-08-04 15:55:54 -0700

    fleshed out db a bit

--- core/Indigo/Db.php
+++ core/Indigo/Db.php
@@ -4,7 +4,26 @@ namespace Indigo;
 
 class Db
 {
-    static public function factory(Config $config)
+    static private $cache = array();
+
+    static public function init(Config $config, $name='default')
+    {
+        self::$cache[$name] = new Db($config);
+    }
+
+    static public function factory(Config $config, $name='default')
+    {
+        if (array_key_exists($name, self::$cache)) {
+            return self::$cache[$name];
+        } else {
+            $e =  new Exception\Db(
+                sprintf('Unknown database "%s"', $name)
+            );
+            throw $e;
+        }
+    }
+
+    public function __construct(Config $config)
     {
         
     }
---
+++ core/Indigo/Exception/Db.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Indigo\Exception;
+
+class Db extends \Exception
+{
+
+}
+
