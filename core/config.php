adf25b0e3a9eb4116542c7e5441bc018e08dfe1d
Steve Phillips <kingcoyote85@gmail.com>
2012-08-04 18:26:13 -0700

    set up basic placeholders for the config data

--- core/config.php
+++ core/config.php
@@ -1,4 +1,6 @@
 <?php
 
-return [];
+return [
+    'charset' => 'utf8',
+];
 
--- sites/all/config.php
+++ sites/all/config.php
@@ -1,4 +1,14 @@
 <?php
 
-return [];
+return [
+    'db' => [
+        'engine'   => 'mysql',
+        'host'     => 'localhost',
+        'port'     => '3306',
+        'username' => 'indigo',
+        'password' => 'indigo',
+    ]
+];
+
+
 
