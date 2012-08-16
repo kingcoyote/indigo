a621636c0fe7ed3eabaecefa724582508d009016
Steve Phillips <kingcoyote85@gmail.com>
2012-08-15 17:28:38 -0700

    added a few new events related to the db

--- core/Indigo/Db.php
+++ core/Indigo/Db.php
@@ -9,12 +9,16 @@ class Db
     
     static public function init(Config $config, $name='default')
     {
+        extract(Event::trigger('indigo db init', ['config' => $config, 'name' => $name]));
+
         self::$cache[$name] = new Db($config);
         return self::$cache[$name];
     }
 
     static public function registerEngine($name, $class)
     {
+        extract(Event::trigger('indigo db registerEngine', ['name' => $name, 'class' => $class]));
+
         if (!class_exists($class)) {
             throw new Exception\Db(
                 sprintf('Engine type "%s" does not exist', $class)
@@ -30,6 +34,8 @@ class Db
 
     static public function factory($name='default')
     {
+        $name = Event::trigger('indigo db factory', $name);
+
         if (array_key_exists($name, self::$cache)) {
             return self::$cache[$name];
         } else {
