65e9d591e8ede9e5058ef35eef63b28a069448d6
Steve Phillips <kingcoyote85@gmail.com>
2012-08-17 15:22:06 -0700

    moved autoload register to before module init, since this can cause a fatal error

--- index.php
+++ index.php
@@ -16,7 +16,9 @@ try {
     $site = Indigo\Site::fetch();    
 
     $loader = new \Composer\Autoload\ClassLoader();
-
+    $loader->add($site, 'sites/');
+    $loader->register();
+    
     // initialize the core of the system based on that site's config
     $config = Indigo\Config::factory($site);
 
@@ -26,9 +28,6 @@ try {
             require_once(INDIGO_DIR . 'modules/' . $module . '/init.php');
         }
     }
-
-    $loader->add($site, 'sites/');
-    $loader->register();
 
     // initialize MySQL in index. This will probably eventually go into a module
     Indigo\Db::registerEngine('mysql', '\\Indigo\\Db\\Mysql');
