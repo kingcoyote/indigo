0afbaaba757823674055750058b982c02a5d5fcc
Steve Phillips <kingcoyote85@gmail.com>
2012-08-31 08:17:06 -0700

    added an article controller for helping build Crimson's MySQL library

---
+++ sites/Nightlife/Controller/Article.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Nightlife\Controller;
+
+use Indigo;
+use Indigo\Template;
+use Indigo\Db;
+
+class Article extends Indigo\Controller
+{
+    public static $routes = [
+        '/article' => [
+            'page' => 'view_all'
+        ],
+        '/article/{id}' => [
+            'page' => 'view'
+        ],
+        '/article/{id}/edit' => [
+            'page' => 'edit'
+        ],
+        '/article/{id}/save' => [
+            'page' => 'save'
+        ],
+    ];
+
+    public function view_all($request)
+    {
+        $query = Db::factory()->createQuery();
+        $query->select()->from('article');
+
+        $articles = $query->execute();
+
+        $template = Template::factory()->createView('article/all');
+        $template->articles = $articles;
+
+        return $template->render();
+    }
+
+    public function view($request)
+    {
+
+    }
+
+    public function edit($request)
+    {
+
+    }
+
+    public function save($request)
+    {
+
+    }
+}
