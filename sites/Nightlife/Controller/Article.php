b33d021296dd30842ca65ff13a41faff745a19b5
Steve Phillips <kingcoyote85@gmail.com>
2012-09-01 08:08:57 -0700

    misc updates to the Article controller

--- sites/Nightlife/Controller/Article.php
+++ sites/Nightlife/Controller/Article.php
@@ -5,6 +5,7 @@ namespace Nightlife\Controller;
 use Indigo;
 use Indigo\Template;
 use Indigo\Db;
+use Indigo\Exception;
 
 class Article extends Indigo\Controller
 {
@@ -16,7 +17,7 @@ class Article extends Indigo\Controller
             'page' => 'view'
         ],
         '/article/{id}/edit' => [
-            'page' => 'edit'
+            'page' => 'edit',
         ],
         '/article/{id}/save' => [
             'page' => 'save'
@@ -55,12 +56,36 @@ class Article extends Indigo\Controller
 
     public function edit($request)
     {
+        $query = Db::factory()->createQuery();
+        $query->select()->from('article')->where('id', '=', $request['args']['id']);
 
+        $articles = $query->execute();
+
+        if ($articles) {
+            $template = Template::factory()->createView('article/edit');
+            $template->article = $articles[0];
+
+            return $template->render();
+        } else {
+            return $this->_article_not_found($request);
+        }
     }
 
     public function save($request)
     {
+        if ($request['method'] != 'POST') {
+            throw new Exception\Auth();
+        }
 
+        $query = Db::factory()->createQuery();
+        $query->update('article')->set($request['post'])->where('id', '=', $request['args']['id']);
+
+        $query->execute(); 
+
+        // this line is absolutely not staying. i just can't yet decide how i want to handle
+        // page redirects
+        header('Location: /article/' . $request['args']['id']);
+        die;
     }
 
     protected function _article_not_found($request)
